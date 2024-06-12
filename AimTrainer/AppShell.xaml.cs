31f8046ced74bd7f64f00db8dc55eaf041de5c3c
Justin Zhu <sukiwhitex7@gmail.com>
2024-06-12 15:08:40 -0600

    Add project files.

---
+++ AimTrainer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AimTrainer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AimTrainer/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace AimTrainer
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void highlight(object sender, EventArgs e)
+        {
+            ImageButton button = (ImageButton)sender;
+            button.BackgroundColor = Color.FromArgb("#bfa100");
+        }
+
+        private void lowlight(object sender, EventArgs e)
+        {
+            ImageButton button = (ImageButton)sender;
+            button.BackgroundColor = Colors.Gray;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+        }
+    }
+
+}
